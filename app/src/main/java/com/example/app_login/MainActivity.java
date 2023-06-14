890e676838c7a034d0ad26f37ffae3ea30bcedb8
Thameera Dananjaya <84010024+Thameera08@users.noreply.github.com>
2023-06-14 18:21:41 +0530

    commit 6

--- app/src/main/java/com/example/app_login/MainActivity.java
+++ app/src/main/java/com/example/app_login/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.app_login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = findViewById(R.id.username);
+        TextView password = findViewById(R.id.password);
+
+        MaterialButton login_btn = findViewById(R.id.login_btn);
+
+        //admin
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("admin") && password.getText().toString().equals("1234")) {
+                    //correct
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESS", Toast.LENGTH_SHORT).show();
+
+                }else
+                    Toast.makeText(MainActivity.this,"LOGIN FAIL",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
